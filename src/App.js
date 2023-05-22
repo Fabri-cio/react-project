54ec5046f00cb1e7a4d40ca39aa9d06acc16ff17
will <wil.fabri777@gmail.com>
2023-05-21 23:35:03 -0400

    my first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+function App() {
+  return (
+    <div>
+      <h1>My first Project in React</h1>
+      <p>Welcome to my Project in React</p>
+    </div>
+  );
+}
+
+export default App;
